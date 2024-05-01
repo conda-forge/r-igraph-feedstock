1af623b2facfdc925912676af47f02700641fb6c
Mervin Fansler <fanslerm@mskcc.org>
2024-05-01 15:21:43 -0400

    revert

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 :: Some SystemRequirements are optional: gmp, libxml2, glpk
 :: Update ./src/{Makevars.win, config.h} based on the availability of the
 :: optional SystemRequirements provided via meta.yaml.
-:: bash ./configure
-:: bash -c "mv src/Makevars src/Makevars.win"
+bash ./configure
+bash -c "mv src/Makevars src/Makevars.win"
 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
