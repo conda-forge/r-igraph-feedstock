9cd2fa7169d846b4a05fbb6fe2b70f242f46c900
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-09-23 15:38:13 +0200

    use Makevars.ucrt

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,2 @@
-:: Some SystemRequirements are optional: gmp, libxml2, glpk
-:: Update ./src/{Makevars.win, config.h} based on the availability of the
-:: optional SystemRequirements provided via meta.yaml.
-bash ./configure
-bash -c "mv src/Makevars src/Makevars.win"
-
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
